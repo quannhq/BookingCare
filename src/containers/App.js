b1acc7b370dd92c9d3f552331cfcbd5e9db75b33
quannhq <nguyenhoangquan.contact@gmail.com>
2024-01-02 21:38:14 +0700

    [Update] App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,10 @@ import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter as Router } from 'connected-react-router';
 import { history } from '../redux'
 import { ToastContainer } from 'react-toastify';
+
+
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
+
 import { path } from '../utils'
 import HomePage from './HomePage/HomePage';
 import DetailDoctor from './Patient/Doctor/DetailDoctor';
